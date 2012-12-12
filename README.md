9edfad905dc3b5c7740ec6b32b0fd17551c4f517
Soldier-Boy <soldierboy2000@gmail.com>
2012-12-12 15:48:08 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-firemap
-=======
+﻿# FireMap
+
+Karte für Feuerwehren
+
+Anzeige von:
+
+* Feuerwehrhäuser + Details
+* Hydranten + Details
+* Wasserstellen + Details
+
+## Technologien
+
+* [Leaflet 0.4.5](http://leafletjs.com/)
+** [Leaflet-Control-Zoomslider](https://github.com/kartena/Leaflet.zoomslider)
+** [Leaflet-Control-Locate](https://github.com/domoritz/leaflet-locatecontrol)
+* [jQuery](http://jquery.com/)
+* [Overpass-API](https://wiki.openstreetmap.org/wiki/Overpass_API)
---
+++ init.js
@@ -0,0 +1,71 @@
+﻿var language = window.navigator.userLanguage || window.navigator.language;
+
+$(function(){
+	var map = initMap();
+	var baseLayers = getBaseLayers();
+	var overlays = getOverlays();
+	addLayers(map,baseLayers,overlays);	
+	addControls(map,baseLayers,overlays);
+	
+	getData(map,overlays);
+});
+
+function initMap(){
+	return new L.Map('map',
+	{
+		minZoom: 0,
+		maxZoom: 18,
+		center: new L.LatLng(0,0),
+		zoom: 3
+	}).locate({
+		setView: true,
+		maxZoom: 12
+	});
+}
+
+function getBaseLayers(){
+	var osm = new L.TileLayer(
+		'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+		{ attribution: 'Map data © OpenStreetMap contributors' }
+	);
+	
+	return {
+		"OpenStreetMap": osm
+	};
+}
+
+function getOverlays(){
+	var feuerwehr = new L.LayerGroup();
+	var hydranten = new L.LayerGroup();
+	var wasser = new L.LayerGroup();
+	
+	return {
+		"Feuerwehr": feuerwehr,
+		"Hydranten": hydranten,
+		"Wasser": wasser
+	};
+}
+
+function addLayers(map,baseLayers,overlays){
+	map.addLayer(baseLayers.OpenStreetMap);
+	
+	map.addLayer(overlays.Feuerwehr);
+	map.addLayer(overlays.Hydranten);
+	map.addLayer(overlays.Wasser);
+}
+
+function addControls(map,baseLayers,overlays){	
+	map.addControl(new L.Control.Scale());
+	map.addControl(new L.Control.Layers(baseLayers,overlays));
+	map.addControl(new L.Control.Locate({follow: true}));
+}
+
+function getData(){
+	getFeuerwehr(map,overlays.Feuerwehr);
+	getHydranten(map,overlays.Hydranten);
+	getWasser(map,overlays.Wasser);
+	
+	map.on('moveend',function(){getFeuerwehr(map,overlays.Feuerwehr)});
+	map.on('moveend',function(){getHydranten(map,overlays.Hydranten)});
+	map.on('moveend',function(){getWasser(map,overlays.Wasser)});
+}
